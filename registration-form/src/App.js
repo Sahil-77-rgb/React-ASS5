71cb59b9c560ebcdf54de79df28807e337dabf43
Sahil-77-rgb <sahilkshirsagar235@gmail.com>
2024-10-20 21:47:51 +0530

    Add files via upload

---
+++ registration-form/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import RegistrationForm from './RegistrationForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Registration Form App</h1>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
